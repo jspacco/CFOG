94d09b2b0745c19fbe038537319fe7d351a8190e
Jaime Spacco <jspacco@knox.edu>
2015-06-21 12:00:19 -0500

    basic, really ugly version of application

--- js/makemap.js
+++ js/makemap.js
@@ -6,31 +6,63 @@ Object.size = function(obj) {
     return size;
 };
 
-function makeJSTree(map) {
+function makeJSTree(map, gvizmap) {
+	// (???) may need to use hasOwnProperty() with for-in loops in JS
+	// http://stackoverflow.com/questions/684672/loop-through-javascript-object
+	// TODO: add to text: Student, Problem, CFOG
+
 	var jstree=[];
 	for (var problem in map) {
 		var current={};
 		jstree.push(current);
-		current['text']=problem;
+		current['text']='PROB: '+problem;
 		var cfglist=[];
 		current['children']=cfglist;
 		for (var cfg in map[problem]) {
 			var cfgchild={};
 			cfglist.push(cfgchild);
-			cfgchild['text']=cfg.replace(' ','');
+			//cfgchild['text']='CFOG: '+cfg.replace(' ','')+ ' GRAPHVIZ: '+gvizmap[cfg];
+			cfgchild['text']='CFOG: '+cfg.replace(' ','');
 			var userlist=[];
 			cfgchild['children']=userlist;
 			for (var user in map[problem][cfg]) {
 				var userchild={};
 				userlist.push(userchild);
-				userchild['text']=user;
+				userchild['text']='USER: '+user;
 				var datalist=[];
 				userchild['children']=datalist;
-				for (var list in map[problem][cfg][user]) {
+
+				tmp = map[problem][cfg][user];
+				for (var i=0; i<tmp.length; i++) {
+					var list=tmp[i];
+					user=list[0];
+					problem=list[1];
+					timestamp=list[2];
+					attempted=list[3];
+					passed=list[4];
+					code=list[5];
+					cfg=list[6];
+					graphviz=list[7].replace(/\\n/g, '\n');
+					cfgchild['data']=[graphviz];
 					var data={}
+					data['text']='TIME: '+timestamp;
 					datalist.push(data);
-					data['text']=list[2];
-					// [user,problem,timestamp,attempted,passed,code,cfg,graphviz]
+					/*
+					data['children']=[{'text':'SCOR: '+passed+'-'+attempted, 
+						'children':[{'text':'CODE: '+code, 
+							'children': [{'text':'GVIZ: '+graphviz}]
+						}]
+					}];
+					*/
+
+					data['data']=[code.replace(/\\n/g, '\n'), graphviz, passed, attempted];
+					
+					/*
+					data['children']=;
+					codechild.push([{'text':code, 'children': [{'text':}]}]);
+					codechild[0]['children']=
+					data['children']=codechild;
+					*/
 				}
 			}
 		}
@@ -43,6 +75,7 @@ function makeJSTree(map) {
 
 function readSubs(text) {
 	var map={};
+	var gvizmap={};
 	var lines=text.split("\n");
 	for (var i=0; i<lines.length; i++) {
 		var line=lines[i];
@@ -67,16 +100,18 @@ function readSubs(text) {
 		}
 		if (!(cfg in map[problem])) {
 			map[problem][cfg] = {};
+			gvizmap[cfg] = graphviz;
 		}
 		if (!(user in map[problem][cfg])) {
 			map[problem][cfg][user] = []
 		}
-		map[problem][cfg][user].push([user,problem,timestamp,attempted,passed,code,cfg,graphviz]);
+		lst=[user,problem,timestamp,attempted,passed,code,cfg,graphviz];
+		map[problem][cfg][user].push(lst);
 	}
 
 
-	var size = Object.size(map);
-	console.log("length of map " + size);
-
+	//var size = Object.size(map);
+	//console.log("length of map " + size);
+	//return {"map" : map, "gvizmap" : gvizmap};
 	return map;
 };
